db9d6c2295a4be7a43139939aff46c8e2e48f4fd
wejustdevelop <wejustdevelop@gmail.com>
2017-10-21 16:46:26 +0800

    base layout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './shared/sections/header/header.component';
+import { NavigationComponent } from './shared/sections/navigation/navigation.component';
+import { SidebarComponent } from './shared/sections/sidebar/sidebar.component';
+import { FooterComponent } from './shared/sections/footer/footer.component';
+import { ContentComponent } from './core/sections/content/content.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooterComponent,
+    HeaderComponent,
+    NavigationComponent,
+    SidebarComponent,
+    ContentComponent
   ],
   imports: [
     BrowserModule
---
+++ src/resources/js/app.js
@@ -0,0 +1,3 @@
+$(document).ready(function(){
+  $.backstretch("/assets/img/i.jpg");
+});
