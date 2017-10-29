af3c7e7b6b2e2663a4ed65ba9078698fd9678e61
villartadh <villartadh@gmail.com>
2017-10-29 18:34:14 +0800

    remove unwanted images

--- src/resources/js/app.js
+++ src/resources/js/app.js
@@ -28,7 +28,7 @@ $(document).ready(function(){
 
   // Collapse Navbar
   var navbarCollapse = function() {
-    if ($("#mainNav").offset().top > 50) {
+    if ($("#mainNav").offset().top > 40) {
       $("#mainNav").addClass("navbar-shrink");
     } else {
       $("#mainNav").removeClass("navbar-shrink");
