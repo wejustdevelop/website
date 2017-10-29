fc06144ff2e1ccd27554fca9a815a7e7bdda45da
villartadh <villartadh@gmail.com>
2017-10-29 21:49:41 +0800

    addded a working basic navigation routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { NgModule } from '@angular/core';
 import { FacebookModule } from 'ngx-facebook';
@@ -8,12 +9,22 @@ import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
+
 import { HeaderComponent } from './shared/sections/header/header.component';
 import { NavigationComponent } from './shared/sections/navigation/navigation.component';
 import { SidebarComponent } from './shared/sections/sidebar/sidebar.component';
 import { FooterComponent } from './shared/sections/footer/footer.component';
 import { ContentComponent } from './core/sections/content/content.component';
 import { ContributorsComponent } from './core/sections/contributors/contributors.component';
+
+import { JobsComponent } from './jobs/jobs.component';
+import { NetworkComponent } from './network/network.component';
+import { ContactUsComponent } from './contact-us/contact-us.component';
+import { AboutUsComponent } from './about-us/about-us.component';
+import { FaqComponent } from './faq/faq.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { HomeComponent } from './home/home.component';
+
 
 @NgModule({
   declarations: [
@@ -23,10 +34,26 @@ import { ContributorsComponent } from './core/sections/contributors/contributors
     NavigationComponent,
     SidebarComponent,
     ContentComponent,
-    ContributorsComponent
+    ContributorsComponent,
+    JobsComponent,
+    NetworkComponent,
+    ContactUsComponent,
+    AboutUsComponent,
+    FaqComponent,
+    NotFoundComponent,
+    HomeComponent
   ],
   imports: [
     FormsModule,
+    RouterModule.forRoot([
+      { path: '', component: HomeComponent },
+      { path: 'jobs', component: JobsComponent },
+      { path: 'network', component: NetworkComponent },
+      { path: 'contact-us', component: ContactUsComponent },
+      { path: 'about-us', component: AboutUsComponent },
+      { path: 'faq', component: FaqComponent },
+      { path: '**', component: NotFoundComponent }
+    ]),
     BrowserModule,
     FacebookModule.forRoot(),
     InfiniteScrollModule,
