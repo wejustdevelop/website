8a359d0cf905447e05431e0a3b0961d7f7095c86
Alex Hoffman <alexhoffman617@gmail.com>
2018-10-03 13:40:44 -0400

    Updated routes to include missing routes and components
    [Issue 27](https://github.com/wejustdevelop/website/issues/27)

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,12 +1,15 @@
-import {RouterModule, Routes} from '@angular/router';
-import {ContactUsComponent} from './contact-us/contact-us.component';
-import {NgModule} from '@angular/core';
-import {ContentComponent} from './core/sections/content/content.component';
-import {UnderDevComponent} from './under-dev.component';
-import {NetworkComponent} from './network.component';
+import { RouterModule, Routes } from '@angular/router';
+import { ContactUsComponent } from './contact-us/contact-us.component';
+import { NgModule } from '@angular/core';
+import { ContentComponent } from './core/sections/content/content.component';
+import { UnderDevComponent } from './under-dev.component';
+import { NetworkComponent } from './network.component';
+import { JobsComponent } from './jobs/jobs.component';
+import { AboutUsComponent } from './about-us/about-us.component';
+import { FaqComponent } from './faq/faq.component';
 
 const routes = [
-  {path: '', redirectTo: '/home', pathMatch: 'full'},
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
   {
     path: 'home',
     component: ContentComponent
@@ -20,15 +23,25 @@ const routes = [
     component: NetworkComponent
   },
   {
+    path: 'jobs',
+    component: JobsComponent
+  },
+  {
+    path: 'about',
+    component: AboutUsComponent
+  },
+  {
+    path: 'faq',
+    component: FaqComponent
+  },
+  {
     path: '**',
     component: UnderDevComponent
-  }];
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-
-export class AppRoutingModule {
-
-}
+export class AppRoutingModule {}
